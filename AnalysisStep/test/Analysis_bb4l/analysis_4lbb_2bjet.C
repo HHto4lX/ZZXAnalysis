59eeade21bd3bf58d6d9ffdbe61bc7a029ea2936
alessandra cappati <alessandra.cappati@cern.ch>
2020-04-03 16:42:05 +0200

    add btag SF

--- AnalysisStep/test/Analysis_bb4l/analysis_4lbb_2bjet.C
+++ AnalysisStep/test/Analysis_bb4l/analysis_4lbb_2bjet.C
@@ -32,7 +32,15 @@
 #include <vector>
 #include "TLorentzVector.h"
 
+
+// includes for btag SF
+#include "../BTAG_reweight/BTagCalibrationStandalone.h"
+#include "../BTAG_reweight/BTagCalibrationStandalone.cpp"
+#include "../BTAG_reweight/evalEventSF.C"
+
+
 #include "CMS_lumi.C"
+
 
 
 using namespace std;
@@ -129,8 +137,141 @@ void doHistos()
 
 
 
+  ///////////// SET UP B-TAG CALIBRATION ///////////////
+    
+  // set up calibration + reader
+  cout << "Loading the .csv file..." << endl;
+    
+  if(year==2016)     { std::string inputCSVfile = "../../data/BTagging/DeepCSV_2016LegacySF_V1.csv"; }
+  else if(year==2017){ std::string inputCSVfile = "../../data/BTagging/DeepCSV_94XSF_V4_B_F.csv"; }
+  else if(year==2018){ std::string inputCSVfile = "../../data/BTagging/DeepCSV_102XSF_V1.csv"; }
+  else{ std::string inputCSVfile = ""; }
+  std::string measType            = "iterativefit";
+  std::string sysType             = "central";
+  std::string sysTypeJESUp        = "up_jes";
+  std::string sysTypeJESDown      = "down_jes";
+  std::string sysTypeHFUp         = "up_hf";
+  std::string sysTypeHFDown       = "down_hf";
+  std::string sysTypeLFUp         = "up_lf";
+  std::string sysTypeLFDown       = "down_lf";
+  std::string sysTypehfstats1Up   = "up_hfstats1";
+  std::string sysTypehfstats1Down = "down_hfstats1";
+  std::string sysTypehfstats2Up   = "up_hfstats2";
+  std::string sysTypehfstats2Down = "down_hfstats2";
+  std::string sysTypelfstats1Up   = "up_lfstats1";
+  std::string sysTypelfstats1Down = "down_lfstats1";
+  std::string sysTypelfstats2Up   = "up_lfstats2";
+  std::string sysTypelfstats2Down = "down_lfstats2";
+  std::string sysTypecfErr1Up     = "up_cferr1";
+  std::string sysTypecfErr1Down   = "down_cferr1";
+  std::string sysTypecfErr2Up     = "up_cferr2";
+  std::string sysTypecfErr2Down   = "down_cferr2";
+    
+  BTagCalibration calib("csvv2", inputCSVfile);
+  //nominal
+  BTagCalibrationReader CSVreader(BTagEntry::OP_RESHAPING, sysType);       
+  CSVreader.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreader.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreader.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_jes shift
+  BTagCalibrationReader CSVreaderJESUp(BTagEntry::OP_RESHAPING, sysTypeJESUp);       
+  CSVreaderJESUp.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderJESUp.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderJESUp.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_jes shift
+  BTagCalibrationReader CSVreaderJESDown(BTagEntry::OP_RESHAPING, sysTypeJESDown);       
+  CSVreaderJESDown.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderJESDown.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderJESDown.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_hf shift
+  BTagCalibrationReader CSVreaderHFUp(BTagEntry::OP_RESHAPING, sysTypeHFUp);       
+  CSVreaderHFUp.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderHFUp.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderHFUp.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_hf shift
+  BTagCalibrationReader CSVreaderHFDown(BTagEntry::OP_RESHAPING, sysTypeHFDown);       
+  CSVreaderHFDown.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderHFDown.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderHFDown.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_lf shift
+  BTagCalibrationReader CSVreaderLFUp(BTagEntry::OP_RESHAPING, sysTypeLFUp);       
+  CSVreaderLFUp.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderLFUp.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderLFUp.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_lf shift
+  BTagCalibrationReader CSVreaderLFDown(BTagEntry::OP_RESHAPING, sysTypeLFDown);       
+  CSVreaderLFDown.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderLFDown.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderLFDown.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_hfstats1 shift
+  BTagCalibrationReader CSVreaderhfstats1Up(BTagEntry::OP_RESHAPING, sysTypehfstats1Up);       
+  CSVreaderhfstats1Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderhfstats1Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderhfstats1Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_hfstats1 shift
+  BTagCalibrationReader CSVreaderhfstats1Down(BTagEntry::OP_RESHAPING, sysTypehfstats1Down);       
+  CSVreaderhfstats1Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderhfstats1Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderhfstats1Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_lfstats2 shift
+  BTagCalibrationReader CSVreaderhfstats2Up(BTagEntry::OP_RESHAPING, sysTypehfstats2Up);       
+  CSVreaderhfstats2Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderhfstats2Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderhfstats2Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_lfstats2 shift
+  BTagCalibrationReader CSVreaderhfstats2Down(BTagEntry::OP_RESHAPING, sysTypehfstats2Down);       
+  CSVreaderhfstats2Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderhfstats2Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderhfstats2Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_lfstats1 shift
+  BTagCalibrationReader CSVreaderlfstats1Up(BTagEntry::OP_RESHAPING, sysTypelfstats1Up);       
+  CSVreaderlfstats1Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderlfstats1Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderlfstats1Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_lfstats1 shift
+  BTagCalibrationReader CSVreaderlfstats1Down(BTagEntry::OP_RESHAPING, sysTypelfstats1Down);       
+  CSVreaderlfstats1Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderlfstats1Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderlfstats1Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_lfstats2 shift
+  BTagCalibrationReader CSVreaderlfstats2Up(BTagEntry::OP_RESHAPING, sysTypelfstats2Up);       
+  CSVreaderlfstats2Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderlfstats2Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderlfstats2Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_lfstats2 shift
+  BTagCalibrationReader CSVreaderlfstats2Down(BTagEntry::OP_RESHAPING, sysTypelfstats2Down);       
+  CSVreaderlfstats2Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreaderlfstats2Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreaderlfstats2Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_cferr1 shift
+  BTagCalibrationReader CSVreadercfErr1Up(BTagEntry::OP_RESHAPING, sysTypecfErr1Up);       
+  CSVreadercfErr1Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreadercfErr1Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreadercfErr1Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_cferr1 shift
+  BTagCalibrationReader CSVreadercfErr1Down(BTagEntry::OP_RESHAPING, sysTypecfErr1Down);       
+  CSVreadercfErr1Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreadercfErr1Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreadercfErr1Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //up_cferr2 shift
+  BTagCalibrationReader CSVreadercfErr2Up(BTagEntry::OP_RESHAPING, sysTypecfErr2Up);       
+  CSVreadercfErr2Up.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreadercfErr2Up.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreadercfErr2Up.load(calib, BTagEntry::FLAV_UDSG, measType);
+  //down_cferr2 shift
+  BTagCalibrationReader CSVreadercfErr2Down(BTagEntry::OP_RESHAPING, sysTypecfErr2Down);       
+  CSVreadercfErr2Down.load(calib, BTagEntry::FLAV_B, measType);
+  CSVreadercfErr2Down.load(calib, BTagEntry::FLAV_C, measType);
+  CSVreadercfErr2Down.load(calib, BTagEntry::FLAV_UDSG, measType);
+    
+  cout << "Input CSV weight file = " << inputCSVfile << "; measurementType = " << measType << ";" << endl;
 
-  // input file branches
+  ///////////////////////////
+
+
+
+
+  // --- input file branches
   TFile* inputFile[nDatasets];
   TTree* inputTree[nDatasets];
   TH1F* hCounters[nDatasets];
@@ -167,8 +308,8 @@ void doHistos()
   vector<Float_t> *JetEta    = 0;
   vector<Float_t> *JetPhi    = 0;
   vector<Float_t> *JetMass   = 0;
-  vector<Float_t> *JetIsBtagged = 0;
   vector<Float_t> *JetBTagger = 0;
+  vector<Short_t> *JetHadronFlavour = 0;
   Float_t PFMET;
 
   // define yields histos and nEvents histos (no weight)
@@ -334,7 +475,7 @@ void doHistos()
     inputTree[d]->SetBranchAddress("JetMass", &JetMass);
     inputTree[d]->SetBranchAddress("JetPhi",  &JetPhi);
     inputTree[d]->SetBranchAddress("JetBTagger",  &JetBTagger);
-    inputTree[d]->SetBranchAddress("JetIsBtagged",  &JetIsBtagged);  
+    inputTree[d]->SetBranchAddress("JetHadronFlavour",  &JetHadronFlavour);
     inputTree[d]->SetBranchAddress("PFMET",  &PFMET);  
     if(currentProcess == ggZZ){
       inputTree[d]->SetBranchAddress("KFactor_QCD_ggZZ_Nominal", &KFactor_QCD_ggZZ_Nominal);
@@ -364,14 +505,20 @@ void doHistos()
       }
 
       
-      // --- fill k factors and event weights
+      // --- fill k factors
       Float_t kfactor = 1.;
       if(currentProcess == qqZZ) { kfactor = KFactor_EW_qqZZ * KFactor_QCD_qqZZ_M; } // qqZZ sample                      
       else if(currentProcess == ggZZ) { kfactor = KFactor_QCD_ggZZ_Nominal; } //ggZZ samples 
 
+      // compute SF
+      double * scaleFactors;
+      scaleFactors = evalEventSF( int(JetPt->size()), JetHadronFlavour, JetEta, JetPt, JetBTagger, CSVreader, CSVreaderJESUp, CSVreaderJESDown, CSVreaderHFUp, CSVreaderHFDown, CSVreaderLFUp, CSVreaderLFDown, CSVreaderhfstats1Up, CSVreaderhfstats1Down, CSVreaderhfstats2Up, CSVreaderhfstats2Down, CSVreaderlfstats1Up, CSVreaderlfstats1Down, CSVreaderlfstats2Up, CSVreaderlfstats2Down, CSVreadercfErr1Up, CSVreadercfErr1Down, CSVreadercfErr2Up, CSVreadercfErr2Down );
+
+      // --- event weights
       Double_t eventWeight = 1.;
-      if(currentProcess != Data && currentProcess != ZXbkg) eventWeight = partialSampleWeight[d] * xsec * kfactor * overallEventWeight * L1prefiringWeight;
+      if(currentProcess != Data && currentProcess != ZXbkg) eventWeight = partialSampleWeight[d] *xsec *kfactor *overallEventWeight *L1prefiringWeight *scaleFactors[0];
       if(currentProcess == ZXbkg) eventWeight = weight; //ZX weight
+
 
 
       // --- select final state
