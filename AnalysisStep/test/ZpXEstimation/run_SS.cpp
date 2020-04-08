8594fd3b070c15d9ddfefc6c082ffc7670c390ba
alessandra cappati <alessandra.cappati@cern.ch>
2020-04-08 15:57:48 +0200

    reproduce 2018 4lsel FR

--- AnalysisStep/test/ZpXEstimation/run_SS.cpp
+++ AnalysisStep/test/ZpXEstimation/run_SS.cpp
@@ -20,12 +20,12 @@ int main( int argc, char *argv[] )
 {
    setTDRStyle();
    
-   TString path = "";
-   TString file_name = "/ZZ4lAnalysis.root";
+   TString path = "/eos/user/a/acappati/samples_HH4lbb/samples_2018/";
+   TString file_name = "/ZZXAnalysis.root";
 	
    TString Data    = path + "AllData"        + file_name;
-   TString WZ      = path + "WZTo3LNu"       + file_name;
-   TString ZZ      = path + "ZZTo4l"         + file_name;
+   TString WZ      = path + "WZTo3LNuext1"   + file_name;
+   TString ZZ      = path + "ZZTo4lext1"     + file_name;
    TString ttbar   = path + "TTTo2L2Nu"      + file_name;
    TString DY      = path + "DYJetsToLL_M50" + file_name;
 	
@@ -36,7 +36,7 @@ int main( int argc, char *argv[] )
    float pT_bins[] = {5, 7, 10, 20, 30, 40, 50, 80};
 
    SSmethod *ss = new SSmethod();
-   ss->SetLumi(41.53);
+   ss->SetLumi(59.7);
 
    ///////////////////////////////////
    // Fill control histos           //
@@ -46,21 +46,21 @@ int main( int argc, char *argv[] )
    ss->FillDataMCPlots(ZZ);
    ss->FillDataMCPlots(ttbar);
    ss->FillDataMCPlots(DY);
-   ss->SaveDataMCHistos("DataMC_SS_Moriond18.root");
+   ss->SaveDataMCHistos("DataMC_SS_samples18.root");
 
    ///////////////////////////////////
    // Fill passing/failling histos  //
    ///////////////////////////////////
    ss->FillFRHistos(Data);
    ss->FillFRHistos(WZ);
-   ss->SaveFRHistos("Histos_SS_Moriond18.root", SubtractWZ, Remove_NegBins_FR);
+   ss->SaveFRHistos("Histos_SS_samples2018.root", SubtractWZ, Remove_NegBins_FR);
 
    ///////////////////////////////////
    // Calculate fake rates          //
    ///////////////////////////////////
-   ss->GetFRHistos("Histos_SS_Moriond18.root");
+   ss->GetFRHistos("Histos_SS_samples2018.root");
    ss->Set_pT_binning(8, pT_bins);
-   ss->ProduceFakeRates("FakeRates_SS_Moriond18.root", Data);
+   ss->ProduceFakeRates("FakeRates_SS_samples2018.root", Data);
 
    ///////////////////////////////////
    // Calculate OS/SS ratios        //
@@ -70,19 +70,19 @@ int main( int argc, char *argv[] )
    ///////////////////////////////////
    // Fill ZX contributions histos  //
    ///////////////////////////////////
-   ss->MakeHistogramsZX(Data, "FakeRates_SS_Moriond18.root");
-   ss->SaveZXHistos("ZXHistos_SS_Moriond18.root");
+   ss->MakeHistogramsZX(Data, "FakeRates_SS_samples2018.root");
+   ss->SaveZXHistos("ZXHistos_SS_samples2018.root");
 
    ///////////////////////////////////
    // Plot control plots            //
    ///////////////////////////////////
-   ss->GetDataMCHistos("DataMC_SS_Moriond18.root");
+   ss->GetDataMCHistos("DataMC_SS_samples2018.root");
    ss->PlotDataMC( "M4l", "Plots" );
 
    ///////////////////////////////////
    // Plot and fit Z+X              //
    ///////////////////////////////////
-   ss->GetZXHistos("ZXHistos_SS_Moriond18.root");
+   ss->GetZXHistos("ZXHistos_SS_samples2018.root");
    ss->PlotZX("M4l", "Plots");
    ss->FitZX("M4l", "Plots");
 	
